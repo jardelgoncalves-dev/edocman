68828c9ca5bdfc24d05a5c100408393fc4bb88f0
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-03-21 16:29:04 -0300

    add error pages

---
+++ Application/views/error/page403.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require '../Application/views/layout/head.php';
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col text-center" style="margin-top:150px">
+        <img src="/assets/images/error403.png" width="200" alt="">
+        <h1>Forbidden!</h1>
+    </div>
+  </div>
+</div>
+
+<?php
+require '../Application/views/layout/footer.php';
+?>
---
+++ Application/views/error/page500.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require '../Application/views/layout/head.php';
+?>
+
+<div class="container">
+  <div class="row">
+    <div class="col text-center" style="margin-top:150px">
+        <img src="/assets/images/error500.png" width="200" alt="">
+        <h1>Internal Server Error!</h1>
+    </div>
+  </div>
+</div>
+
+<?php
+require '../Application/views/layout/footer.php';
+?>

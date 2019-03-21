7a3540991c7eb74dc02fa05db8f60af88cf77664
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-03-21 00:04:07 -0300

    modifying the way to instantiate the model

--- Application/repository/Base.php
+++ Application/repository/Base.php
@@ -8,6 +8,7 @@ class Base
 
   protected $validator = null;
   protected $response = array();
+  protected $model = null;
   
   
   public function __construct(Validator $validator)
@@ -18,31 +19,17 @@ class Base
   private function attrValueDefault($variable, $default) {
     return $variable === null ? $default : $variable;
   }
-
-  private function model($model)
-  {
-    if (class_exists('Application\\models\\' . $model)) {
-      $model = 'Application\\models\\' . $model;
-      return new $model();
-    } else {
-      throw new Exception("Class not found!", 6000);
-    }
-  }
-
-  public function call_method(string $model, string $method, array $params = array(),
+  
+  public function call_method($instance, string $method, $param = null,
                                 int $code = 200, $message_error = 'Not found!')
   {
     $code = $this->attrValueDefault($code, 200);
     $message_error = $this->attrValueDefault($message_error, 'Not found');
-    $params = $this->attrValueDefault($params, array());
-
-    $message_error = $message_error === null ?  200 : $code;
+    $param = $this->attrValueDefault($param, null);
 
     if (!$this->validator->getErrors()) {
-        $intance = $this->model($model);
-    
-        if (method_exists($intance, $method)) {
-            $result = call_user_func([$intance, $method], $params);
+        if (method_exists($instance, $method)) {
+            $result = call_user_func([$instance, $method], $param);
             if (!$result) {
                 $code = 404;
                 $this->response = $message_error;

353e9a5fb326e4db02ff15ca6a8ebd81b46c42bb
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-03-21 16:28:03 -0300

    add header and footer files from pages

---
+++ Application/views/layout/footer.php
@@ -0,0 +1,4 @@
+  <script src="/assets/js/master.js"></script>
+</body>
+
+</html>
---
+++ Application/views/layout/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>EdocMan</title>
+  <link rel="stylesheet" href="/assets/css/style.css">
+  <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+
+<body>

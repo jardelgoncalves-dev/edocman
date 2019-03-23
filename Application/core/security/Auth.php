a2023d856b950e2bb1b174915fd55a9345d3245b
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-03-22 22:05:38 -0300

    returning user as an object

--- Application/core/security/Auth.php
+++ Application/core/security/Auth.php
@@ -4,6 +4,7 @@ namespace Application\core\security;
 
 use Application\core\session\Session;
 use Application\core\http\Response;
+
 class Auth extends Session
 {
 
@@ -12,11 +13,12 @@ class Auth extends Session
   public static function get_user()
   {
     self::init();
-    return self::has('user') ? self::get('user') : false;
+    return self::has('user') ? (object) self::get('user') : false;
   }
 
   public static function onlyAuthenticated()
   {
+    self::init();
     if (!self::has('user')) {
       Response::redirect(self::$to_login_page);
     }

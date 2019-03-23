5c227c534ad20aa22279651f83ff8a78bb3e5d20
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-03-23 00:52:21 -0300

    add menu dashboard

---
+++ Application/views/layout/menu.php
@@ -0,0 +1,20 @@
+  <nav>
+    <input type="checkbox" id="nav" class="hidden">
+    <label for="nav" class="nav-btn">
+      <i></i>
+      <i></i>
+      <i></i>
+    </label>
+    <div class="logo">
+      <a href="#">EDOCMAN</a>
+    </div>
+    <div class="nav-wrapper">
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="documents.html">Documents</a></li>
+        <li><a href="tags.html">Tags</a></li>
+        <li><a href="users.html">Users</a></li>
+        <li><a href="/page/logout">Logout</a></li>
+      </ul>
+    </div>
+  </nav>

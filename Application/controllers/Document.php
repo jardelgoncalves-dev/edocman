4b694ad47709426f5f6d46919ba2e876976691b3
Jardel Gonçalves <jardelgoncalves1996@gmail.com>
2019-03-25 20:24:16 -0300

    correcting logic to save document and searching files by word (in db and using tesseract)

--- Application/controllers/Document.php
+++ Application/controllers/Document.php
@@ -7,6 +7,7 @@ use Application\core\Controller;
 use Application\core\http\Response;
 use Application\repository\Base;
 use Application\helpers\Validator;
+use Application\helpers\Miscellaneous;
 class Document extends Controller
 {
   protected $response = null;
@@ -40,16 +41,26 @@ class Document extends Controller
   {
     $validator = new Validator(array(
       'filename.required' => @$_POST['filename'],
-      'size.required'     => @$_POST['size'],
-      'size.float'        => @$_POST['size'],
+      'size.required'     => @$_FILES['image']['size'],
+      'size.float'        => @$_FILES['image']['size'],
       'author.required'   => @$_POST['author'],
       'tag_id.required'   => @$_POST['tag_id'],
       'tag_id.integer'    => @$_POST['tag_id'],
-      'image.required'    => @$_POST['image']
+      'image.required'    => @$_FILES['image']['tmp_name'],
+      'image.file'        => @$_FILES['image']['tmp_name']
     ));
 
+    $document = array(
+      'filename' => @$_POST['filename'],
+      'size'     => @$_FILES['image']['size'],
+      'author'   => @$_POST['author'],
+      'tag_id'   => @$_POST['tag_id'],
+      'image'    => Miscellaneous::getBase64FromFile(@$_FILES['image']['tmp_name']),
+    );
+    
+
     $RepositoryBase = new Base($validator);
-    $data = $RepositoryBase->call_method($this->model('Documents'), 'save', $_POST, 201);
+    $data = $RepositoryBase->call_method($this->model('Documents'), 'save', $document, 201);
     $this->response->statusCode($data['code'])->setContentType('application/json')
                    ->body($data['response'])->sendJSON();
   }
@@ -63,6 +74,32 @@ class Document extends Controller
 
     $RepositoryBase = new Base($validator);
     $data = $RepositoryBase->call_method($this->model('Documents'), 'delete', $id, 200, 'Document not found!');
+    $this->response->statusCode($data['code'])->setContentType('application/json')
+                   ->body($data['response'])->sendJSON();
+  }
+
+  public function search($keyword = null, $type = 'simple')
+  {
+    $validator = new Validator(array(
+      'keyword.required' => $keyword,
+      'type.required'    => $type
+    ));
+    $RepositoryBase = new Base($validator);
+    $data = $RepositoryBase->call_method($this->model('Documents'), 'searchDocument',
+                                  array($keyword, $type), 200, 'Document not found!');
+    $this->response->statusCode($data['code'])->setContentType('application/json')
+                   ->body($data['response'])->sendJSON();
+  }
+
+  public function tag($id = null)
+  {
+    $validator = new Validator(array(
+      'id.required' => $id,
+      'id.integer'  => $id
+    ));
+
+    $RepositoryBase = new Base($validator);
+    $data = $RepositoryBase->call_method($this->model('Documents'), 'findByTagId', $id, 200, 'Document not found!');
     $this->response->statusCode($data['code'])->setContentType('application/json')
                    ->body($data['response'])->sendJSON();
   }
